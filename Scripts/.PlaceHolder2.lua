a66b5de1db45711815a934e40592a41757595ca6
rockumk <rockum@aol.com>
2022-11-27 20:21:05 -0600

    Delete .PlaceHolder2.lua

--- Scripts/.PlaceHolder2.lua
+++
@@ -1,8 +0,0 @@
--- @description Place Holder to prompt creation of the User Folder
--- @author rockumk (Rock Kennedy)
--- @version 1.1
--- @about
---   This does nothing but assure that the User_Data folder is created.
-
-
-reaper.ShowConsoleMsg 'This is a dummy script simply here to prompt the creation of the User_Data directory by ReaPack.'
