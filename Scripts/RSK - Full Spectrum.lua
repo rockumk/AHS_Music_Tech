c7f937dadb1f7bc26e3d70aa36ae31e6b36e0d81
rockumk <rockum@aol.com>
2025-12-03 20:49:46 -0600

    Add files via upload

--- Scripts/RSK - Full Spectrum.lua
+++ Scripts/RSK - Full Spectrum.lua
@@ -1,118 +1,79 @@
 -- @description RSK_Full_Spectrum
--- @version 1.0.1
+-- @version 1.0.2
 -- @author Rock Kennedy
 -- @about
 --   # RSK_Full_Spectrum
 --   Duplicates all pitches at every octave.
+--   Run this from inside the MIDI Editor.
 -- @changelog
---   Name Change
-noteIndex = 0
-------------------------------------------     VARIABLES
-noteExists = 1
-noteIsSelected = 1
-noteIsmuted = 0
-theStartppqpos = 0
-theEndppqpos = 0
-theChan = 1
-thePitch = 1
-theVel = 127
-OneNoteDataRow = {}
-theWholeTable = {""}
+--   + CRITICAL: Removed recursive logic that caused Stack Overflow crashes on long tracks.
+--   + Optimization: Added MIDI_DisableSort for significantly faster generation.
+--   + Safety: Localized all variables.
 
---  ----------------------------------------    FUNCTION LIST
---JUST PRINT AND LINE RETURN MESSAGES
-function msg(theMessage)
-  messageWLF = theMessage.."\n"
-  reaper.ShowConsoleMsg(messageWLF)
+--  ----------------------------------------    MAIN LOGIC
+local function main()
+    -- 1. Get the Active Take from the MIDI Editor
+    local focusedEditorWin = reaper.MIDIEditor_GetActive()
+    if not focusedEditorWin then return end -- Exit if no editor is open
+    
+    local focusedtake = reaper.MIDIEditor_EnumTakes(focusedEditorWin, 0, 1)
+    if not focusedtake or not reaper.ValidatePtr2(0, focusedtake, "MediaItem_Take*") then return end
+
+    -- 2. Setup Variables
+    local theWholeTable = {}
+    
+    -- 3. Count notes (Much faster than guessing)
+    local _, noteCount = reaper.MIDI_CountEvts(focusedtake)
+
+    -- 4. Collect all notes (Using a Loop, not Recursion)
+    for i = 0, noteCount - 1 do
+        local retval, selected, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(focusedtake, i)
+        if retval then
+            table.insert(theWholeTable, {
+                selected = selected,
+                muted = muted,
+                start = startppq,
+                ending = endppq,
+                chan = chan,
+                pitch = pitch,
+                vel = vel
+            })
+        end
+    end
+
+    -- 5. Processing
+    reaper.Undo_BeginBlock()
+    reaper.MIDI_DisableSort(focusedtake) -- Disable sorting for speed
+
+    -- Delete old notes (backwards loop)
+    for i = noteCount - 1, 0, -1 do
+        reaper.MIDI_DeleteNote(focusedtake, i)
+    end
+
+    -- Insert new copies across all octaves
+    for _, noteData in ipairs(theWholeTable) do
+        local base_pitch_class = noteData.pitch % 12
+        local current_pitch = base_pitch_class
+        
+        while current_pitch <= 127 do
+            reaper.MIDI_InsertNote(
+                focusedtake, 
+                noteData.selected, 
+                noteData.muted, 
+                noteData.start, 
+                noteData.ending, 
+                noteData.chan, 
+                current_pitch, 
+                noteData.vel, 
+                false -- noSort
+            )
+            current_pitch = current_pitch + 12
+        end
+    end
+
+    reaper.MIDI_Sort(focusedtake)
+    reaper.Undo_EndBlock("RSK Full Spectrum Generate", -1)
 end
 
-
---COLLECT ALL THE CURRENT NOTE DATA
-function GettheNotes()
-noteExists, noteIsSelected, noteIsmuted, theStartppqpos, theEndppqpos, theChan, thePitch, theVel = reaper.MIDI_GetNote( focusedtake, noteIndex)
-if noteExists then
-OneNoteDataRow = ""
-OneNoteDataRow = {noteExists, noteIsSelected, noteIsmuted, theStartppqpos, theEndppqpos, theChan, thePitch, theVel}
-theWholeTable[#theWholeTable+1] = OneNoteDataRow
---msg("The Note = "..tostring(thePitch).." "..tostring(theVel))
-noteIndex = noteIndex +1
-GettheNotes()
-else 
-end
-end
-
---  -------------------------------------------- PROGRAM ! ! !
-
-
---  SAVE THE DATA ON THE NOTES IN A TABLE
-focusedEditorWin = reaper.MIDIEditor_GetActive()
-focusedtake = reaper.MIDIEditor_EnumTakes(focusedEditorWin,0,1)
---msg(tostring(focusedtake))
-reaper.MIDI_DisableSort(focusedtake)
-GettheNotes()
-
-
---  DELETE THE NOTES
-for i=#theWholeTable,1,-1 do  
-reaper.MIDI_DeleteNote( focusedtake, i-1 )
-end
-
---  PUT IN ALL THE COPIES
-for i=1,#theWholeTable,1 do 
-if theWholeTable[i][1] == true then
---msg("DATA = "..i..", "..tostring(theWholeTable[i][1])..", "..tostring(theWholeTable[i][2])..", "..tostring(theWholeTable[i][3])..", "..tostring(theWholeTable[i][7])..", "..tostring(theWholeTable[i][8]))
---msg("Modulus = "..theWholeTable[i][7]%12)
-theModulus = theWholeTable[i][7]%12
-while theModulus <= 127 do
-reaper.MIDI_InsertNote( focusedtake, 0, 0, theWholeTable[i][4], theWholeTable[i][5], theWholeTable[i][6], theModulus, theWholeTable[i][8], 0 )
-theModulus = theModulus +12
-end
-else
-end
-end
-
-
-
-reaper.MIDI_Sort( focusedtake )
-
---[[
-
-
-
-
-
-
-
-
-
-
-
-for i=2,#theWholeTable-1,1 do 
-msg(tostring(i-1))
-msg(tostring(theWholeTable[i][8]))
-theModulus =(theWholeTable[i][8])%12
-msg("the Modulus = "..theModulus)
-end
-
-
-for i=1, #theWholeTable, 1 do
-if theWholeTable[i][2] then
-
-reaper.MIDI_InsertNote( focusedtake, 0, 0, theWholeTable[i][4], theWholeTable[i][5], theWholeTable[i][6], theWholeTable[i][7]+12, theWholeTable[i][8], 0 )
-
-
-
-end
-end
-
---end
-end
-
-
-
-
-
-
---]]
-
-
+-- Run the script
+main()
