60ed98a5880484195491fd728194c99d6155f415
rockumk <rockum@aol.com>
2026-04-05 09:12:10 -0500

    Update version number to 1.6.8

--- Scripts/FX_Graffiti_Mac.lua
+++ Scripts/FX_Graffiti_Mac.lua
@@ -1,6 +1,6 @@
 -- @description FX Graffiti
 -- @author Rock Kennedy
--- @version 1.6.7
+-- @version 1.6.8
 -- @about
 --   A ReaScript to draw and overlay custom shapes/graffiti on FX windows.
 --   Features include importing/exporting overlays, customizable shapes (circles, squares, outlines),
